197acd69caeab9d1f61ea7664a75ddef3061a0f2
elvis-brandao <elvis.brandao@outlook.com>
2022-02-10 11:09:09 -0300

    Models usuario e comentarios ok

---
+++ models/Comentario.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define(
+        'Comentario',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            texto: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            usuarios_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            posts_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        },
+        {
+            tableName: 'comentarios',
+            timestamps: false
+        }
+    );
+};
---
+++ models/Usuario.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define(
+        'Usuario',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            senha: {
+                type: DataTypes.STRING,
+                allowNull: false
+            }
+        },
+        {
+            tableName: 'usuarios',
+            timestamps: false
+        }
+    );
+};
